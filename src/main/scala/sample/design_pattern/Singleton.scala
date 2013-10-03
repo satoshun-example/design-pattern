e8969a1561c4abd49b0a3c5d798c3a48b3a8c740
SatoShun <shun.sato1@gmail.com>
2013-10-03 13:37:52 +0900

    Fixed singleton scala # Please enter the commit message for your changes. Lines starting # with '#' will be kept; you may remove them yourself if you want to. # An empty message aborts the commit. # On branch master # Changes to be committed: #   (use "git reset HEAD <file>..." to unstage) # #	modified:   src/main/scala/sample/design_pattern/Singleton.scala # # Untracked files: #   (use "git add <file>..." to include in what will be committed) # #	src/main/c/State #	src/main/scala/sample/design_pattern/ChainOfResponsibility.scala

--- src/main/scala/sample/design_pattern/Singleton.scala
+++ src/main/scala/sample/design_pattern/Singleton.scala
@@ -9,11 +9,16 @@ package sample.design_pattern;
 
 object SingletonClass {
     val value = 100
+
+    def getSize() = {
+        value * value
+    }
 }
 
 
 object Singleton {
     def main(args: Array[String]) = {
         println(SingletonClass.value)
+        println(SingletonClass.getSize())
     }
 }
